5f8498f0af8e826528095bf0e2a8a30b646661f4
sunslife <brightway2005@126.com>
2020-04-23 10:09:30 +0800

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,10 +28,7 @@ self.addEventListener('activate', function (event) {
       return caches.open(CACHE_NAME);
     }).then(function (cache) {
       console.log("sw adding RESOURCES..."); //it will throw exception "Uncaught (in promise) TypeError: Request failed".
-      return cache.addAll(Object.keys(RESOURCES));
-    }).then(function (cache) {
-      console.log("sw adding RESOURCES2...");
-      return cache.addAll(Object.keys(RESOURCES2));
+      return cache.addAll(Object.keys(RESOURCES).concat(Object.keys(RESOURCES2)));
     })
   );
 });
